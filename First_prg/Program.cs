b086bf7ba1f296957c76efa1f21d9b86e788e690
Rajputana <Rajputana@JASWAL-SAAB>
2022-09-17 10:10:03 +0530

    Add project files.

---
+++ First_prg/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_prg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("abhay ");
+            //Circle c = new Circle(10);
+            //c.Area();
+            //Console.WriteLine(c);
+
+            Rectangle r = new Rectangle(15, 10);
+            r.Area();
+            Console.WriteLine(r);
+        }
+    }
+}
---
+++ First_prg/Shapes.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_prg
+{
+    abstract public class Shapes
+    {
+        public abstract void Area();
+    }
+
+    public class Circle: Shapes
+    {
+        private double radius;
+        private double result;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override void Area()
+        {
+            result = 3.14 * radius * radius;
+        }
+
+        public override string ToString()
+        {
+            return $"Area of Circle -> {result}";  
+        }
+
+    }
+    
+    public class Rectangle : Shapes
+    {
+        private double length, breadth;
+        private double area_r;
+
+        public Rectangle(double length, double breadth)
+        {
+            this.length = length;
+            this.breadth = breadth;
+        }
+
+        public override void Area()
+        {
+            area_r = length * breadth;
+        }
+
+        public override string ToString()
+        {
+            return $" Area of Rectangle -> {area_r}";
+        }
+
+    }
+}
